2906543d7d00767f23611d3f2319fbe22d72d635
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-19 21:14:25 +0700

    feat: add Header component

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,15 @@
+const Header = ({title, description, classname}: { title: string, description: string, classname?: string }) => {
+    return (
+        <header className={`${classname}`}>
+            <span className="h-[4px] w-14 block bg-secondary mb-4 rounded"></span>
+            <h2 className="text-2xl font-semibold">
+                {title}
+            </h2>
+            <p>
+                {description}
+            </p>
+        </header>
+    )
+}
+
+export default Header;
