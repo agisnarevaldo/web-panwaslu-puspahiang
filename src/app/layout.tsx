1734e8930bcc1ce9f82a88cc6e5d1e74dd775c89
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-19 21:16:00 +0700

    refactor: optimize layout component and add Navbar
    - Optimize the layout component for better performance
    - Add the Navbar component to the layout for navigation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Navbar from "@/components/navigation";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -26,8 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white  dark:bg-[#0a0a0a] text-[#171717] dark:text-[#ededed]`}
       >
+        <Navbar />
         {children}
       </body>
     </html>
