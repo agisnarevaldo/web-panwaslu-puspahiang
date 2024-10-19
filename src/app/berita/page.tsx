f5fcfa8aad6fbf579fd2775d43e7afd038133e94
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-19 21:15:29 +0700

    feat: add Berita page component with Carousel

---
+++ src/app/berita/page.tsx
@@ -0,0 +1,9 @@
+import Carousel from "@/components/carousel";
+
+export default function Page() {
+  return (
+    <div className="min-h-screen flex flex-col items-center ">
+      <Carousel />
+    </div>
+  );
+}
