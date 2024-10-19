33a1e7eca1df08f136b1231bfd9dc2b5b494a4d5
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-19 21:14:44 +0700

    style: update Navbar styling

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -4,7 +4,7 @@ import NavLink from "./navLink";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between items-center w-full px-12 py-4">
+    <nav className="flex justify-between items-center w-full px-12 py-1 sticky top-0 z-50 bg-gray-100 dark:bg-gray-900 bg-opacity-80 backdrop-blur-lg">
       <Logo />
       <div className="flex items-center">
         <NavLink href="/" icon="teenyicons:home-outline" text="Beranda" />
