d8d96dee9d297b027f6a7e11576029a951bd76e8
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-20 21:13:43 +0700

    feat: Add Card component for show "visi misi"

---
+++ src/components/ui/card.tsx
@@ -0,0 +1,12 @@
+export default function Card({title, children, classname}: {
+    title: string,
+    children: React.ReactNode
+    classname?: string,
+}) {
+    return (
+        <div className={`${classname} flex flex-col border border-secondary py-4 px-6 rounded-lg gap-3`}>
+            <h1 className="text-xl font-semibold">{title}</h1>
+            {children}
+        </div>
+    );
+}
