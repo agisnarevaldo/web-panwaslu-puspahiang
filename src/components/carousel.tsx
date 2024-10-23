b55d738bc8a51e49bf1a61116003af9f8c6d87b4
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-23 23:30:17 +0700

    refactor: remove unused import

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -3,7 +3,6 @@
 import {Icon} from "@iconify/react/dist/iconify.js";
 import Image from "next/image";
 import {useEffect, useState} from "react";
-import Link from "next/link";
 import Button from "@/components/ui/buttonPrimary";
 
 type NavigateProps = {
