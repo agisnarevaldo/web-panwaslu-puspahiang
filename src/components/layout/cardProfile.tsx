2fa1fd68beb07644ddccfb00a90ef5bba7784016
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-23 23:31:25 +0700

    feat: create cardProfile component to show profile of staff, the carProfile use card component

---
+++ src/components/layout/cardProfile.tsx
@@ -0,0 +1,47 @@
+import Image from "next/image";
+import Logo from "@/components/logo";
+import Card from "@/components/ui/card";
+
+interface CardProfileProps {
+    position: string;
+    image: string;
+    name: string;
+    period: string;
+    address: string;
+    phone: string;
+}
+
+const CardProfile = ({position, image, name, period, address, phone}: CardProfileProps) => {
+    return (
+        <Card title={position} classname="flex-col w-max rounded-xl">
+            <div className="flex gap-4">
+                <Image
+                    src={image}
+                    alt={position}
+                    width={200}
+                    height={200}
+                    className="rounded-lg"
+                />
+                <div className="flex flex-col justify-between">
+                    <div>
+                        <P>Nama: {name}</P>
+                        <P>Jabatan: {position}</P>
+                        <P>Periode: {period}</P>
+                        <P>Alamat: {address}</P>
+                        <P>No. Telp: {phone}</P>
+                    </div>
+
+                    <div className="flex justify-end">
+                        <Logo/>
+                    </div>
+                </div>
+            </div>
+        </Card>
+    )
+}
+
+const P = ({children}: {children: React.ReactNode}) => {
+    return <p className="text-lg">{children}</p>
+}
+
+export default CardProfile;
