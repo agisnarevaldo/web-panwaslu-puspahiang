4e289850dd064ea7e9ca98b906e78567f3466778
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-28 23:45:21 +0700

    refactor: add section for layout berita page

--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -1,9 +1,22 @@
-import Carousel from "@/components/carousel";
+import Section from "@/components/layout/section";
+import CardBerita from "@/components/ui/cardBerita";
 
 export default function Page() {
   return (
-    <div className="min-h-screen flex flex-col items-center ">
-      <Carousel />
-    </div>
+    <main>
+        <Section title="Berita" description="Berita terbaru seputar dunia teknologi">
+            <div className="">
+                <CardBerita
+                    classname="flex-row w-full gap-4"
+                    title={"Judul Berita"}
+                    image={"/1.png"}
+                    date={"2021-08-01"}
+                    author={"Admin"}
+                    href={"/berita/judul-berita"}
+                    description={"Deskripsi panjang sekali tentang berita ini. Deskripsi panjang sekali tentang berita ini. Deskripsi panjang sekali tentang berita ini."}
+                />
+            </div>
+        </Section>
+    </main>
   );
 }
