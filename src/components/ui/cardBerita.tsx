8257a358024e8a1da04107a09eb8a0fc796b45fe
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-28 23:44:53 +0700

    refactor: add additional cardProps: classname for more dynamically

--- src/components/ui/cardBerita.tsx
+++ src/components/ui/cardBerita.tsx
@@ -8,13 +8,14 @@ interface CardBeritaProps {
     author: string;
     href: string;
     description: string;
+    classname?: string;
 }
 
-export default function CardBerita( {title, image, date, author, href, description}: CardBeritaProps ) {
+export default function CardBerita( {title, image, date, author, href, description, classname}: CardBeritaProps ) {
     return (
         <Link
             href={href}
-            className="flex flex-col p-4 rounded-lg shadow-md dark:shadow-slate-700 my-4 w-80 hover:shadow-xl border border-transparent dark:hover:border-gray-600 hover:border hover:border-gray-400 transition-shadow duration-300"
+            className={`flex ${classname} p-4 rounded-lg shadow-md dark:shadow-slate-700 my-4 w-80 hover:shadow-xl border border-transparent dark:hover:border-gray-600 hover:border hover:border-gray-400 transition-shadow duration-300`}
         >
             <Image
                 src={image}
@@ -23,9 +24,11 @@ export default function CardBerita( {title, image, date, author, href, descripti
                 height={200}
                 className="rounded-lg mb-2"
             />
-            <p className="text-sm text-gray-500">{date} - {author}</p>
-            <h2 className="text-lg font-bold -tracking-tight line-clamp-2">{title}</h2>
-            <p className="text-gray-600 line-clamp-3">{description}</p>
+            <div>
+                <p className="text-sm text-gray-500">{date} - {author}</p>
+                <h2 className="text-lg font-bold -tracking-tight line-clamp-2">{title}</h2>
+                <p className="text-gray-600 line-clamp-3">{description}</p>
+            </div>
         </Link>
     )
 }
