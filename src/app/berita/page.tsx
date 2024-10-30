ae11ebd58bdc8c0432db693f94a22734240a011e
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-30 20:52:30 +0700

    refactor: implement sectionBerita

--- src/app/berita/page.tsx
+++ src/app/berita/page.tsx
@@ -1,22 +1,9 @@
-import Section from "@/components/layout/section";
-import CardBerita from "@/components/ui/cardBerita";
+import SectionTopBerita from "@/components/layout/sectionTopBerita";
 
 export default function Page() {
   return (
     <main>
-        <Section title="Berita" description="Berita terbaru seputar dunia teknologi">
-            <div className="">
-                <CardBerita
-                    classname="flex-row w-full gap-4"
-                    title={"Judul Berita"}
-                    image={"/1.png"}
-                    date={"2021-08-01"}
-                    author={"Admin"}
-                    href={"/berita/judul-berita"}
-                    description={"Deskripsi panjang sekali tentang berita ini. Deskripsi panjang sekali tentang berita ini. Deskripsi panjang sekali tentang berita ini."}
-                />
-            </div>
-        </Section>
+        <SectionTopBerita/>
     </main>
   );
 }
