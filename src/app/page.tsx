cb3d82251f7ea22a573098ae104ebb3900697416
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-30 20:52:22 +0700

    refactor: implement sectionBerita

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,58 +1,15 @@
 "use client";
 
 import Carousel from "@/components/carousel";
-import CardBerita from "@/components/ui/cardBerita";
-import Section from "@/components/layout/section";
-import Button from "@/components/ui/buttonPrimary";
 import CountDown from "@/components/ui/countDown";
+import SectionTopBerita from "@/components/layout/sectionTopBerita";
 
 export default function Home() {
     return (
         <div className="min-h-screen flex flex-col items-center ">
             <Carousel/>
 
-            <Section
-                // classname="border border-gray-200"
-                title="Berita Terbaru"
-                description="Berita terbaru seputar pemilu 2024"
-            >
-                <div className="flex flex-wrap justify-between">
-                    <CardBerita
-                        classname="flex-col"
-                        title={"Pemilu 2024"}
-                        image={"/bawaslu.png"}
-                        date={"2024-08-08"}
-                        description={"Pemilu 2024 akan segera digelar, berbagai persiapan telah dilakukan oleh KPU dan Bawaslu"}
-                        author={"Admin"}
-                        href={"/berita/pemilu-2024"}
-                    />
-                    <CardBerita
-                        classname="flex-col"
-                        title={"Pemilu 2024"}
-                        image={"/bawaslu.png"}
-                        date={"2024-08-08"}
-                        description={"Pemilu 2024 akan segera digelar, berbagai persiapan telah dilakukan oleh KPU dan Bawaslu"}
-                        author={"Admin"}
-                        href={"/berita/pemilu-2024"}
-                    />
-                    <CardBerita
-                        classname="flex-col"
-                        title={"Pemilu 2024"}
-                        image={"/bawaslu.png"}
-                        date={"2024-08-08"}
-                        description={"Pemilu 2024 akan segera digelar, berbagai persiapan telah dilakukan oleh KPU dan Bawaslu"}
-                        author={"Admin"}
-                        href={"/berita/pemilu-2024"}
-                    />
-                </div>
-                <div className="flex justify-end mt-4">
-                    <Button
-                        href="/berita"
-                        classname="w-max"
-                        txt="Lihat Berita Lainnya"
-                    />
-                </div>
-            </Section>
+            <SectionTopBerita/>
 
             <CountDown targetDate={new Date("2024-11-27")}/>
         </div>
