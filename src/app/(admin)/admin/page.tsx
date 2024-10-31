a7bac8517fbcc3e158d4b4e83634e1528c4477ae
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-10-31 17:48:54 +0700

    refactor: group main layout

---
+++ src/app/(admin)/admin/page.tsx
@@ -0,0 +1,9 @@
+import {Button} from "@/components/ui/button";
+
+export default function Page() {
+    return (
+        <div className="bg-blue-500">
+            <Button></Button>
+        </div>
+    )
+}
