0d6428d121cbd6d7ad01d52332cd52410cf89ae0
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-11-01 17:11:54 +0700

    refactor: show data TableBerita on berita page

--- src/app/(admin)/admin/berita/page.tsx
+++ src/app/(admin)/admin/berita/page.tsx
@@ -1,9 +1,10 @@
-import FormPostBerita from "@/components/admin/formPostBerita";
+// import FormPostBerita from "@/components/admin/formPostBerita";
+import TabelBerita from "@/components/admin/tabelBerita";
 
 export default function Page() {
     return (
         <div className="container mx-auto mt-8">
-            <FormPostBerita />
+            <TabelBerita />
         </div>
     )
 }
