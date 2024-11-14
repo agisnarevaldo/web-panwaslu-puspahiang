370a8cdd44b363cb1e7a74273d065cf369a99d88
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-11-14 17:50:53 +0700

    refactor: change style

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,7 +1,7 @@
 const Header = ({title, description, classname}: { title: string, description?: string, classname?: string }) => {
     return (
         <header className={`${classname}`}>
-            <span className="h-[4px] w-14 block bg-yelow mb-4 rounded"></span>
+            <span className="h-[4px] w-14 block bg-yellow mb-4 rounded"></span>
             <h2 className="text-2xl font-semibold">
                 {title}
             </h2>
