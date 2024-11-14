c20c829d216d355a2cacf7507fdecc06089f1e5d
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-11-14 17:24:43 +0700

    refactor: change style

--- src/components/ui/cardOld.tsx
+++ src/components/ui/cardOld.tsx
@@ -4,7 +4,7 @@ export default function CardOld({title, children, classname}: {
     classname?: string,
 }) {
     return (
-        <div className={`${classname} flex border border-secondary py-4 px-6 rounded-lg gap-3`}>
+        <div className={`${classname} flex border border-yellow py-4 px-6 rounded-lg gap-3`}>
             <h1 className="text-xl font-semibold">{title}</h1>
             {children}
         </div>
