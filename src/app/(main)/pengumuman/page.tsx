2090b45f3699498ad951b92d17d590b675633f5f
Agisna Revaldo <agisrifaldo75@gmail.com>
2024-11-16 22:11:34 +0700

    feat: create page for show list pengumuman user side

---
+++ src/app/(main)/pengumuman/page.tsx
@@ -0,0 +1,9 @@
+import SectionListPengumuman from "@/components/layout/sectionListPengumuman";
+
+export default function Page() {
+    return (
+        <main>
+            <SectionListPengumuman/>
+        </main>
+    )
+}
